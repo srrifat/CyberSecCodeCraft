6abeaa787ddc17a4ed368c7b26da9e962d0eee31
srrifat <srrifat781@gmail.com>
2024-05-12 08:58:47 +0600

    Add salting program

---
+++ salting.c
@@ -0,0 +1,23 @@
+//Salting
+//Find the salted from of a password entered by user if the salt is "123" & added at the end.
+
+#include<stdio.h>
+#include<string.h>
+
+void salting(char password[]);
+
+int main(){
+    char password[100];
+    scanf("%s",password);
+    salting(password);
+
+}
+
+void salting(char password[]){
+    char salt[] ="123";
+    char newPass[200];
+
+    strcpy(newPass, password); // newPass = "test"
+    strcat(newPass,salt); //newPass ="test" + "123" ;
+    puts(newPass);
+}
